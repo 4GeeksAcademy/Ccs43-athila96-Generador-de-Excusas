48d607381e464099ac788a61d75638ec6f2acd04
Samuel Herrera <futbol.ingeniero@gmail.com>
2023-05-31 22:40:26 +0000

    Se generan excusas cada vez que se recarga la pagina

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  function generadorDeExcusas() {
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    let sujeto = who[Math.floor(Math.random() * who.length)];
+    let verbo = action[Math.floor(Math.random() * action.length)];
+    let predicado = what[Math.floor(Math.random() * what.length)];
+    let predicado2 = when[Math.floor(Math.random() * when.length)];
+
+    return sujeto + " " + verbo + " " + predicado + " " + predicado2;
+  }
+
+  const elemento = document.querySelector("#excuse");
+  elemento.innerText = generadorDeExcusas();
 };
